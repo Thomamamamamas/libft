65808ddbce51c6fd635aceb8b82a12dd7d3a4dae
Thomamamamamas <thomascasale06@gmail.com>
2021-09-21 11:20:03 +0200

    fin premiere partie

---
+++ srcs/ft_strlcat.c
@@ -0,0 +1,54 @@
+#include <stddef.h>
+
+char	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i = i + 1;
+	return (i);
+}
+
+size_t	*ft_strlcat(char *dest, char *src, size_t size)
+{
+	size_t	len_dest;
+	size_t	len_src;
+	size_t	i;
+
+	i = 0;
+	len_dest = ft_strlen(dest);
+	len_src = ft_strlen(src);
+	while ((i < len_src) && (src[i]) && i < size)
+	{
+		dest[len_dest + i] = src[i];
+		i = i + 1;
+	}
+	dest[len_dest + i] = '\0';
+	return (dest);
+}
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int	main()
+{
+	char	*dest;
+	char	*src;
+
+	dest = malloc(sizeof (char) * 7);
+	src = malloc(sizeof (char) * 8);
+	dest[0] = 'p';
+	dest[1] = 'u';
+	dest[2] = 't';
+	dest[3] = 'e';
+	src[0] = 'p';
+	src[1] = 'u';
+	src[2] = 't';
+	src[3] = 'a';
+	src[4] = 's';
+	src[5] = 's';
+	src[6] = 'e';
+	printf("%d\n", strlcat(dest, src, 5));
+}
+

73cd661e6f641fdc74a1e2ce191d20f6362d2a9d
Thomas Casale <tcasale@student.42nice.fr>
2022-08-13 14:33:08 +0200

    modify Makefile

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: tcasale <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/20 12:57:17 by tcasale           #+#    #+#             */
-/*   Updated: 2021/11/13 15:35:24 by tcasale          ###   ########.fr       */
+/*   Updated: 2021/11/14 08:27:50 by tcasale          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,14 +27,10 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	n = 0;
 	m = 0;
 	while (s1[m])
-	{
 		str[n++] = s1[m++];
-	}
 	m = 0;
 	while (s2[m])
-	{
 		str[n++] = s2[m++];
-	}
 	str[n] = '\0';
 	return (str);
 }
